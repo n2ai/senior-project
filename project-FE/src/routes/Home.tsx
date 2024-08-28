a92d00d7e4316b094bdc4988dbafb0bf659a80a9
n2ai <longhai2511@gmail.com>
2024-08-28 12:50:52 -0500

    Update GI section switch task feature

--- project-FE/src/routes/Home.tsx
+++ project-FE/src/routes/Home.tsx
@@ -6,11 +6,12 @@ import riceFiledImg from "../images/rice-field-7218033_1920.jpg";
 import saigonRiverImg from "../images/saigon-river-4593234_1920.jpg";
 import streetVendorImg from "../images/street-vendor-4176310_1920.jpg";
 import nightImg from "../images/night-4639069_1920.jpg"
-import { useState, useEffect } from "react";
+import { useState, useEffect, ReactElement } from "react";
 import Carousel from 'react-bootstrap/Carousel';
 const Home = ()=>{
 
     const [navOpen, setNavOpen] = useState<boolean>(false);
+    const [guideInfoIndex, setGuideInfoIndex] = useState<number>(0);
 
     const images:string[] = [
         bridgeImg,
@@ -58,8 +59,55 @@ const Home = ()=>{
         )
     })
 
+    const guideInfoArray:{id:number,buttonName:string,information:string, icon:ReactElement}[] = [{
+        id:0,
+        buttonName:"What is this?",
+        information:"What is this description",
+        icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+                </svg>
+    },{
+        id:1,
+        buttonName:"What topic?",
+        information:"What topic description",
+        icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+            </svg>
+                  
+    },{
+        id:2,
+        buttonName:"Our Goal",
+        information:"Our Goal Description",
+        icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+    }]
+
+    const guideInfoButtonClickHandler = (index:number):void=>{
+        setGuideInfoIndex(index)
+    }
+
+    const guideInfoButtonArray = guideInfoArray.map((item)=>{
+        return (<button className="w-[12.5rem] flex justify-center" id={String(item.id)} onClick={()=>guideInfoButtonClickHandler(item.id)}>
+            {item.icon}
+            <p>{item.buttonName}</p>
+        </button>)
+    })
+
     const hamburgerOnClick = ():void=>{
         setNavOpen((prev)=>!prev)
+    }
+
+    const leftButtonGuideInfo = ():void=>{
+        setGuideInfoIndex((prev)=>{
+            return (Math.abs((prev-1) % guideInfoArray.length)) 
+        })
+    }
+
+    const rightButtonGuideInfo = ():void=>{
+        setGuideInfoIndex((prev)=>{
+            return ((prev+1) % guideInfoArray.length) 
+        })
     }
 
     return(
@@ -132,6 +180,44 @@ const Home = ()=>{
                     <div className= "mt-2">
                         <div>
                             <h1 className="text-3xl">General Information</h1>
+                        </div>
+                        
+                        
+                        <div className="mt-4">
+
+                            {/**Mobile*/}
+                            {/**Banner Button */}
+                            <div className="w-full flex items-center justify-between sm:hidden" >
+                                <button onClick={leftButtonGuideInfo}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                                    </svg>
+                                        
+                                </button>
+                                <div className="w-[12.5rem] flex justify-center">
+                                    {guideInfoArray[guideInfoIndex].icon}
+
+                                    <p>{guideInfoArray[guideInfoIndex].buttonName}</p>
+                                </div>
+                                <button onClick={rightButtonGuideInfo}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                                    </svg>
+                                </button>
+                            </div>
+
+                            {/**Desktop */}
+                            <div className=" w-full max-sm:hidden flex justify-around">
+                                {guideInfoButtonArray}
+                            </div>
+
+
+                            {/**Information */}
+                            <div className="w-full">
+                                <p className="transition-opacity duration-300">
+                                    {guideInfoArray[guideInfoIndex].information}
+                                </p>
+                            </div>
                         </div>
 
                         
