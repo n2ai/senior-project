b9b71cbd42c476b5b81025a9a149e7cf16846855
n2ai <longhai2511@gmail.com>
2024-10-01 22:47:02 -0500

    Newest

--- project-BE/routes/userCityRoutes.js
+++ project-BE/routes/userCityRoutes.js
@@ -3,5 +3,5 @@ import { handleGetQuizContents } from "../controllers/userCityControllers.js";
 const router = Router();
 
 router.get("/:id/:cityId", handleGetQuizContents);
-
+router.post("/:id/:cit")
 export default router;
